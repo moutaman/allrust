a930331b9ae5706173ca8955decb52008f4de173
M. B <5953462+moutaman@users.noreply.github.com>
2022-01-28 11:51:18 +0300

    calculator

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rust 101
+
+## Basic command line calculator
+
+following Engineer man tutorial [Let's lear rust: checkpoint, create Calcualtor App](https://www.youtube.com/watch?v=RYTMn_kLItw)
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+// import stdin lib to handle user input from command line
+use std::io::{stdin, stdout, Write};
+
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+
+fn handle(input: &mut String) {
+    // handle function to deal with user input 
+    // accepts mutable string variable named input
+    stdout().flush()
+        .expect("failed to flush");
+    stdin().read_line(input)
+        .expect("faild to handle");
+}
+
 fn main() {
-    println!("Hello, world!");
+    // create a mutiable variables to hold user inputs
+
+    loop {
+        let mut var1 = String::new();
+        let mut var2 = String::new();
+        let mut operator = String::new();
+
+        println!("First Number: ");
+        handle(&mut var1);
+
+        println!("Second Number: ");
+        handle(&mut var2);
+
+        println!("Operation: [+-*/] ");
+        handle(&mut operator);
+
+        // Casting string vars to f32 type before executing math operations
+        let _var1: f32 = var1.trim().parse().unwrap();
+        let _var2: f32 = var2.trim().parse().unwrap();
+        let operator: char = operator.trim().parse().unwrap();
+
+        let operators = String::from("+-*/");
+
+        if !operators.contains(operator){
+            println!("Error, {} is not allowed operator", operator);
+            continue;
+        }
+
+        let result = match operator {
+            '+' => _var1 + _var2,
+            '-' => _var1 - _var2,
+            '*' => _var1 * _var2,
+            '/' => _var1 / _var2,
+            _ => panic!("error in operator")
+        };
+
+        println!("{} {} {} = {}", _var1, operator, var2, result);
+    }
 }
